b21cad9936fa1c73e11c6f4f4fd0168bbbc7907d
Dafne Duda <dafne.leslie@gmail.com>
2024-04-22 11:52:40 -0300

    CRUD de categoria e produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import './App.css';
-import Home from './pages/home/Home';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[77.5vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,23 +1,35 @@
-import { Link } from "react-router-dom"
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 function Navbar() {
- 
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
   
 
   return (
     <>
-     <div className='w-full bg-blue-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-purple-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmácia Duda</div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Farmácia Duda</Link>
 
             <div className='flex gap-4'>
-            <Link to='/login' className='hover:underline'>Login</Link>
-            <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Cadastrar categoria</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+            <div className='hover:underline'>Produtos</div>
+            <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+             
             </div>
           </div>
         </div>
